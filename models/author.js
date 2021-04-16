6f883130c347c0bc46a01dcc3576410109989d2a
Reese <arter.charise@gmail.com>
2021-04-16 09:46:40 -0500

    Added author model

---
+++ models/author.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const authorSchema = new Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+});
+
+module.exports = mongoose.model("Author", authorSchema);
--- routes/authors.js
+++ routes/authors.js
@@ -1,23 +1,20 @@
 const express = require("express");
 const router = express.Router();
-
+const Author = require("../models/author");
 
 // All Authors Route
 router.get("/", (req, res) => {
-	res.render("authors/index");
+	res.render("authors/index", { author: new Author() });
 });
-
 
 // New Author route
 router.get("/new", (req, res) => {
 	res.render("authors/new");
 });
 
-
 // Create Author route
 router.post("/", (req, res) => {
 	res.send("Create");
 });
-
 
 module.exports = router;
